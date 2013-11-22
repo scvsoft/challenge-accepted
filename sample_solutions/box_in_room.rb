bf830ff047b253e4ab51cd1d2226f7dded9c253e
Sergio Rafael Gianazza <sgianazza@gmail.com>
2013-11-21 22:08:56 -0300

    Improve solution code

--- sample_solutions/box_in_room.rb
+++ sample_solutions/box_in_room.rb
@@ -1,22 +1,28 @@
+def box_layout(room_size, boxes)
+  room_volume = volume(room_size)
+
+  boxes_amount = []
+
+  boxes.each do |box|
+    box_volume = volume(box)
+    box_quantity = room_volume / box_volume
+
+    if box_quantity == box_quantity.to_i
+      boxes_amount << box_quantity.to_i
+    else
+      boxes_amount << 0
+    end
+  end
+
+  maximum = boxes_amount.max
+  boxes_amount.map { |amount| (maximum == amount) ? amount : 0 }.join(',')
+end
+
+def volume(dimensions)
+  dimensions.split(',').map(&:to_f).reduce(:*)
+end
+
 room_size =  ARGV.shift
 boxes = ARGV
 
-room_volume = room_size.split(',').map(&:to_f).reduce(:*)
-
-boxes_amount = []
-
-# Dumy way
-boxes.each do |box|
-  box_volume = box.split(',').map(&:to_f).reduce(:*)
-  box_quantity = room_volume / box_volume
-  if box_quantity == box_quantity.to_i
-    # No queda espacio libre
-    boxes_amount << box_quantity.to_i
-  else
-    boxes_amount << 0
-  end
-end
-
-# Find the maximum, set the rest to 0
-maximum = boxes_amount.max
-puts boxes_amount.map { |amount| (maximum == amount) ? amount : 0 }.join(',')
+puts box_layout(room_size, boxes)
