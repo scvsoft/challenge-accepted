02419f0c8fe438690ab6b1d62c2113226a40bf8b
Esteban Rodriguez <tepenrodriguez@gmail.com>
2013-11-22 15:37:40 -0300

    apply timeout to all executions

--- ch-specs/executor.rb
+++ ch-specs/executor.rb
@@ -12,14 +12,14 @@ class Executor
 
     args_string = args.join(' ')
 
-    case @type
-    when "ruby"
-      Timeout::timeout(1) do
+    Timeout::timeout(1) do
+      case @type
+      when "ruby"
         `ruby -T3 #{code_path} #{args_string}`.strip
+      when "js"
+        open(code_path, 'a') { |f| f.puts "phantom.exit();" }
+        `phantomjs #{code_path} #{args_string}`.strip
       end
-    when "js"
-      open(code_path, 'a') { |f| f.puts "phantom.exit();" }
-      `phantomjs #{code_path} #{args_string}`.strip
     end
   end
 end
