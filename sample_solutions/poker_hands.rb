c56d019a2c7ad6e03fe132bac317d960773f3992
Esteban Rodriguez <tepenrodriguez@gmail.com>
2013-12-04 17:10:11 -0300

    deobfuscated solution code and added comments

--- sample_solutions/poker_hands.rb
+++ sample_solutions/poker_hands.rb
@@ -1,7 +1,23 @@
 def poker_hand(cards)
-  f=/1{5}|1{4}0+1$/
-  s=[0]*13
-  Hash[*cards.map{|c|s['23456789DJQKA'.index c[0]]+=1;c[1]}.uniq[1]?[f,'escalera',?4,'poker',/3.*2|2.*3/,'full',?3,'pierna',/2.*2/,'doble par',?2,'par',0,'carta mas alta']:[/1{5}$/,'escalera real',f,'escalera de color',0,'color']].find{|r,y|s.join[r]}[1]
+  straight = /1{5}|1{4}0+1$/ # Escalera
+  card_count=[0]*13
+  # Count the cards of each rank into card_count and check for all cards of the same suit
+  not_same_suit = cards.map do |card|
+    card_count['23456789DJQKA'.index card[0]]+=1
+    card[1]
+  end.uniq[1]
+
+  solution_hash = {}
+
+  if not_same_suit
+    # Build hash with regexp for each hand
+    solution_hash = Hash[straight,'escalera',?4,'poker',/3.*2|2.*3/,'full',?3,'pierna',/2.*2/,'doble par',?2,'par',0,'carta mas alta']
+  else
+    # Build hash with regexp for each hand
+    solution_hash = Hash[/1{5}$/,'escalera real',straight,'escalera de color',0,'color']
+  end
+  # apply regexp for solution and return hand string
+  solution_hash.find{|r,y|card_count.join[r]}[1]
 end
 
 cards = ARGV
