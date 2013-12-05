e97d9998b52228688da02d8080acb9217729b294
Sergio Rafael Gianazza <sgianazza@gmail.com>
2013-12-05 18:01:47 -0300

    Added begin and rescue

--- scripts/send_mail.rb
+++ scripts/send_mail.rb
@@ -22,13 +22,13 @@ Si antes o durante el concurso tenes alguna duda, podes dejar un issue en este r
 Gracias por estar participando y muchos éxitos.
 MESSAGE_END
 
-  # begin
-  Net::SMTP.enable_tls(OpenSSL::SSL::VERIFY_NONE)
-  Net::SMTP.start('smtp.gmail.com', 587, 'scvsoft.com', 'bigbrother@scvsoft.com', '5CV50ftR0ck5!', :plain) do |smtp|
-    smtp.send_message str_message, 'bigbrother@scvsoft.com', registration.email
-  end
-  # rescue Exception => e
+  begin
+    Net::SMTP.enable_tls(OpenSSL::SSL::VERIFY_NONE)
+    Net::SMTP.start('smtp.gmail.com', 587, 'scvsoft.com', 'bigbrother@scvsoft.com', '5CV50ftR0ck5!', :plain) do |smtp|
+      smtp.send_message str_message, 'bigbrother@scvsoft.com', registration.email
+    end
+  rescue Exception => e
     # Ignore any exception, continue with the following
-    # puts "Exception: #{e}"
-  # end
+    puts "Exception: #{e}"
+  end
 end
